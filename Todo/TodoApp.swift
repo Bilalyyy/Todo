7e3642b41ac380c0cdfaa3330887c4880cd5f5aa
Bilal <yyybilal@gmail.com>
2021-12-29 23:00:46 +0100

    Initial Commit

---
+++ Todo/TodoApp.swift
@@ -0,0 +1,17 @@
+//
+//  TodoApp.swift
+//  Todo
+//
+//  Created by bilal on 29/12/2021.
+//
+
+import SwiftUI
+
+@main
+struct TodoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
